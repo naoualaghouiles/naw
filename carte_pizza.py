5451cd6ab021be2286c208c63719fe83e4fe925f
Aghouiles_Naoual <aghouilesnaoual0@gmail.com>
2022-03-08 15:37:52 +0100

    les test

--- carte_pizza.py
+++ carte_pizza.py
@@ -1,43 +1,44 @@
-from hashlib import new
-from pickle import APPEND
+"""isEmpty methode"""
 
 
 class PizzaCarte:
-    def __init__(self, pizza=[]) :
+    """isEmpty methode"""
+    def __init__(self, pizza= None) :
+        """constructeur"""
         self.__pizza=pizza
-    
+
     @property
     def pizza(self):
         """The pizza property."""
         return self.__pizza
     @pizza.setter
     def pizza(self, value):
+        """setter"""
         self.__pizza = value
-        
+
     def is_empty(self):
+        """isEmpty methode"""
         return not self.__pizza
-    
+
     def nombre(self):
+        """isEmpty methode"""
         return len(self.pizza)
-     
-        
+
+
     def add_pizza(self, pizza) :
+        """isEmpty methode"""
         self.__pizza.append(pizza)
-        
-        
+
     def remove_pizza(self, pizza):
+        """isEmpty methode"""
         try:
             self.__pizza.remove(pizza)
         except ValueError:
             Exception("Pizza not found")
+
+# if __name__ == "__main__":
+#     p= PizzaCarte()
+#     p.pizza=[1,2,3]
+#     print(p.is_empty())
+#     print(p.nombre())
     
-if __name__ == "__main__":
-    p= PizzaCarte()
-    p.pizza=[1,2,3]
-    print(p.is_empty())
-    
-    print(p.nombre())
-    
-    
-        
-        
--- test_carte.py
+++ test_carte.py
@@ -1,33 +1,37 @@
-from cmath import pi
+"""isEmpty methode"""
 from unittest.mock import Mock
 from carte_pizza import PizzaCarte
 
 
-def test_isEmpty():
+def test_is_empty():
+    """isEmpty methode"""
     carte= PizzaCarte()
     carte.pizza=[]
-    assert  carte.is_empty()
-    
-    
+    assert carte.is_empty()
+
+
 def test_nombre():
+    """isEmpty methode"""
     carte= PizzaCarte()
     carte.pizza=[1,4,5,6]
     assert carte.nombre()==4
-    
+
 def test_add_pizza() :
+    """isEmpty methode"""
     pizza=Mock()
     carte= PizzaCarte()
     carte.pizza=[]
     carte.add_pizza(pizza)
-    
-    assert  carte.pizza==[pizza]    
-    
-    
+
+    assert carte.pizza==[pizza]
+
+
 def test_remove_pizza():
+    """isEmpty methode"""
     pizza=Mock()
     carte= PizzaCarte()
     carte.pizza=[pizza]
-    
+
     carte.remove_pizza(pizza)
+    assert not carte.pizza
     
-    assert  carte.pizza==[]
